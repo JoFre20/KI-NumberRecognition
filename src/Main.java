6dc8f664df9a9c5d593645cb039078436ac899fd
JoFre20 <jontewalter@googlemail.com>
2022-11-14 22:28:45 +0100

    First Push of the NumberRecognition witch uses the Model from the Training

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+import MNISTUtilis.MnistImage;
+import MNISTUtilis.MnistUtil;
+import NeuralNetwork.Layer;
+import NeuralNetwork.NeuralNetwork;
+import NeuralNetwork.StatUtil;
+import Utilis.FileHelper;
+import Utilis.JsonHelper;
+
+public class Main {
+	
+	private static NeuralNetwork neuralnetwork = new NeuralNetwork();
+	
+	public static FileHelper fileHelper = new FileHelper();
+	
+	public static MnistUtil MUtil;
+	
+	public static void main(String[] args) throws IOException {
+		fileHelper.init();
+		MUtil = new MnistUtil();
+		String Modeljson = fileHelper.readFromDisk("files/Model.json");
+		neuralnetwork.init(JsonHelper.JsonToClass(Modeljson, Layer[].class));
+		for (MnistImage mnistimg : MUtil.getImages(1)) {
+			System.out.println("============");
+            System.out.println("KI-Output: " + neuralnetwork.recognitionNumber(StatUtil.ArrayListtoFloatArray(mnistimg.getImgdata())));
+            System.out.println("Ex-Output: " + mnistimg.getLabel());
+            System.out.println("============");
+           
+		}
+		System.out.println("");
+	}
+
+}
---
+++ src/NeuralNetwork/NeuralNetwork.java
@@ -0,0 +1,58 @@
+package NeuralNetwork;
+
+import java.util.HashMap;
+
+import Utilis.JsonHelper;
+
+public class NeuralNetwork {
+	
+	private static Layer[] layers;
+	
+	public static void init(Layer[] layersinput) {
+		layers = layersinput;
+	}
+	
+	public static String recognitionNumber(float[] datainput) {
+        System.out.println("0 -- " + layers[3].neurons[0].value);
+        System.out.println("1 -- " + layers[3].neurons[1].value);
+        System.out.println("2 -- " + layers[3].neurons[2].value);
+        System.out.println("3 -- " + layers[3].neurons[3].value);
+        System.out.println("4 -- " + layers[3].neurons[4].value);
+        System.out.println("5 -- " + layers[3].neurons[5].value);
+        System.out.println("6 -- " + layers[3].neurons[6].value);
+        System.out.println("7 -- " + layers[3].neurons[7].value);
+        System.out.println("8 -- " + layers[3].neurons[8].value);
+        System.out.println("9 -- " + layers[3].neurons[9].value);
+    	HashMap<Float, String> outputs = new HashMap<Float, String>();
+        forward(datainput);
+        outputs.put(layers[3].neurons[0].value, "0");
+        outputs.put(layers[3].neurons[1].value, "1");
+        outputs.put(layers[3].neurons[2].value, "2");
+        outputs.put(layers[3].neurons[3].value, "3");
+        outputs.put(layers[3].neurons[4].value, "4");
+        outputs.put(layers[3].neurons[5].value, "5");
+        outputs.put(layers[3].neurons[6].value, "6");
+        outputs.put(layers[3].neurons[7].value, "7");
+        outputs.put(layers[3].neurons[8].value, "8");
+        outputs.put(layers[3].neurons[9].value, "9");
+        
+        System.out.println(JsonHelper.ClassToJson(layers));
+        return StatUtil.getMaxEntryInMapBasedOnKey(outputs).getValue();
+	}
+    
+    public static void forward(float[] inputs) {
+    	// First bring the inputs into the input layer layers[0]
+    	layers[0] = new Layer(inputs);
+    	
+        for(int i = 1; i < layers.length; i++) {
+        	for(int j = 0; j < layers[i].neurons.length; j++) {
+        		float sum = 0;
+        		for(int k = 0; k < layers[i-1].neurons.length; k++) {
+        			sum += layers[i-1].neurons[k].value*layers[i].neurons[j].weights[k];
+        		}
+        		sum += layers[i].neurons[j].bias; // TODO add in the bias
+        		layers[i].neurons[j].value = StatUtil.Sigmoid(sum);
+        	}
+        } 	
+    }
+}
